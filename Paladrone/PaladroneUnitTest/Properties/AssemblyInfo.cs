2f5b1eac27d702f1a3224584313aff1f658a4b89
Manjinder Singh <manjinder360@yahoo.com>
2018-04-23 10:58:34 -0400

    Initial commit of the paladrone code challenge

---
+++ Paladrone/PaladroneUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PaladroneUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaladroneUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f7c5ae2-89ea-4fae-8653-fbb372f804fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Paladrone/PaladroneUnitTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Libraries;
+
+namespace PaladroneUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void isPaladroneEmptyTestMethod1()
+        {
+            Paladrone paladrone = new Paladrone();
+            bool expected = true;
+            bool actual = paladrone.isPaladrone();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isPaladroneRacecarTestMethod1()
+        {
+            Paladrone paladrone = new Paladrone("racecar");
+            bool expected = true;
+            bool actual = paladrone.isPaladrone();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isPaladrone1221TestMethod1()
+        {
+            Paladrone paladrone = new Paladrone("1221");
+            bool expected = true;
+            bool actual = paladrone.isPaladrone();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isPaladroneEvenOddTestMethod1()
+        {
+            Paladrone paladrone = new Paladrone("never Odd or Even");
+            bool expected = true;
+            bool actual = paladrone.isPaladrone();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isPaladroneabcTestMethod1()
+        {
+            Paladrone paladrone = new Paladrone("abc");
+            bool expected = false;
+            bool actual = paladrone.isPaladrone();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
